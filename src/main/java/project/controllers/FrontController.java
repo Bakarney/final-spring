f9403c81f91b956482e787731513edb03611a93f
Artur Naberezhnyi <artur.naberegniy@gmail.com>
2021-09-17 21:58:07 +0300

    This code doesn't works

--- src/main/java/project/controllers/FrontController.java
+++ src/main/java/project/controllers/FrontController.java
@@ -1,7 +1,15 @@
 package project.controllers;
 
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import project.controllers.commands.Command;
 
 @Controller
 public class FrontController {
@@ -9,5 +17,15 @@ public class FrontController {
 	@GetMapping("/home")
 	public String getHome() {
 		return "home";
+	}
+	
+	@GetMapping("/catalog")
+	public String getCatalog(HttpServletRequest request, Model model) {
+		try {
+			Command.execute(request, model);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "catalog";
 	}
 }
--- src/main/java/project/controllers/Test.java
+++ src/main/java/project/controllers/Test.java
@@ -4,26 +4,29 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import project.DAO.*;
+import project.config.SpringConfig;
 import project.entities.*;
 
 @Controller
 public class Test {
 	
-	private final OrderDAO dao;
-	
-	@Autowired
-	public Test(OrderDAO dao) {
-		this.dao = dao;
-	}
-	
 	@GetMapping("/test")
 	public String test(Model model) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+		ProductDAO dao = context.getBean(ProductDAO.class);
+		context.close();
 		System.out.println("Test servlet");
+		try {
+			System.out.println(dao.getFiltered(null, null, null, null, null, null, null, null));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return "Test";
 	}
 }
